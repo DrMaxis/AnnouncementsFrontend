8ff1ddcec7115a87b204e4d810d9cd5ebf8e5c83
Alchemist <5355808+DrMaxis@users.noreply.github.com>
2023-11-30 01:51:16 -0700

    Formatting

--- pages/api/announcements/index.js
+++ pages/api/announcements/index.js
@@ -2,25 +2,25 @@ import axios from 'axios';
 
 async function FetchAnnouncements(req, res) {
 
-    let customHeaders = {
-        Accept: 'application/json',
-    };
+  let customHeaders = {
+    Accept: 'application/json',
+  };
 
-    return await axios
-        .get(`${process.env.NEXT_PUBLIC_API_URL}/announcements`, {
-            headers: customHeaders,
-        })
-        .then((response) => {
-            return res
-                .status(200)
-                .json({data: response.data.data, message: response.data.message});
-        })
-        .catch((e) => {
-            return res.status(e.response.status).json({
-                errors: e.response.data.errors,
-                message: e.response.data.message,
-            });
-        });
+  return await axios
+    .get(`${process.env.NEXT_PUBLIC_API_URL}/announcements`, {
+      headers: customHeaders,
+    })
+    .then((response) => {
+      return res
+        .status(200)
+        .json({data: response.data.data, message: response.data.message});
+    })
+    .catch((e) => {
+      return res.status(e.response.status).json({
+        errors: e.response.data.errors,
+        message: e.response.data.message,
+      });
+    });
 }
 
 export default FetchAnnouncements;
